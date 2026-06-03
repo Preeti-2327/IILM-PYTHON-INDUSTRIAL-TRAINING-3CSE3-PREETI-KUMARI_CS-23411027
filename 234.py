415ca87c7cc936e8fac93020f9983a8b9f5d00e0
Preeti-2327 <preeti.kumari.cs27@iilm.edu>
2026-06-03 19:34:29 +0530

    Create 234.py

---
+++ 234.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+
+        if not head or not head.next:
+            return True
+
+        # Step 1: Find middle
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Step 2: Reverse second half
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+
+        # Step 3: Compare both halves
+        left = head
+        right = prev
+
+        while right:  # only need to check second half
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+
+        return True

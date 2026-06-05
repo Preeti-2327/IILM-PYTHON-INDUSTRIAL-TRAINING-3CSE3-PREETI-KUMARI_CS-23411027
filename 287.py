06c5f3815bff4a2518496f497d0ff3c97a619c30
Preeti-2327 <preeti.kumari.cs27@iilm.edu>
2026-06-05 20:10:59 +0530

    Create 287.py

---
+++ 287.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        slow = nums[0]
+        fast = nums[0]
+
+        # Find intersection point
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        # Find entrance of cycle
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow

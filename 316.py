eb09cac1478f8823d8fd9f61425188228f5f0410
Preeti-2327 <preeti.kumari.cs27@iilm.edu>
2026-06-06 20:09:34 +0530

    Create 316.py

---
+++ 316.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        last = {c: i for i, c in enumerate(s)}
+        stack = []
+        seen = set()
+
+        for i, c in enumerate(s):
+            if c in seen:
+                continue
+
+            while stack and c < stack[-1] and last[stack[-1]] > i:
+                seen.remove(stack.pop())
+
+            stack.append(c)
+            seen.add(c)
+
+        return "".join(stack)
